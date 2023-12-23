602933b42cd722aaecc3cc26baf5d5cc9f1f8e04
Rafael Silveira <rafaellsilveira1357@gmail.com>
2023-12-22 21:47:55 -0300

    Adicionando times ao groupsConfig.cjs

--- groupsConfig.cjs
+++ groupsConfig.cjs
@@ -10,6 +10,78 @@ const groupsConfig = [
             fontColor: 'white',
         },
     },
+    {
+        groupName: 'Fluminense',
+        aliases: ['fluminense'],
+        styles: {
+            groupImage: 'fluminense.jpg',
+            backgroundColor: 'green',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'Palmeiras',
+        aliases: ['palmeiras'],
+        styles: {
+            groupImage: 'palmeiras.jpg',
+            backgroundColor: 'green',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'São Paulo',
+        aliases: ['são paulo', 'sao paulo', 'sãopaulo', 'saopaulo'],
+        styles: {
+            groupImage: 'saopaulo.jpg',
+            backgroundColor: 'red',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'Vasco',
+        aliases: ['vasco'],
+        styles: {
+            groupImage: 'vasco.jpg',
+            backgroundColor: 'black',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'Internacional',
+        aliases: ['internacional'],
+        styles: {
+            groupImage: 'internacional.jpg',
+            backgroundColor: 'red',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'Botafogo',
+        aliases: ['botafogo', 'bota fogo'],
+        styles: {
+            groupImage: 'botafogo.jpg',
+            backgroundColor: 'black',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'Corinthians',
+        aliases: ['corinthians'],
+        styles: {
+            groupImage: 'corinthians.jpg',
+            backgroundColor: '#1d1d1d',
+            fontColor: 'white',
+        },
+    },
+    {
+        groupName: 'Santos',
+        aliases: ['santos'],
+        styles: {
+            groupImage: 'santos.jpg',
+            backgroundColor: '#1d1d1d',
+            fontColor: 'white',
+        },
+    },
 ];
 
 groupsConfig.forEach(({ groupName, aliases, styles }) => new Group(groupName, aliases, styles));
