1b359075a894173c504b7ce0bbc22403e342245a
SandroRDS <sandror.desouza847@gmail.com>
2023-12-26 23:21:01 -0300

    Implementando callback onGift

--- src/liveConnection/onGift.cjs
+++ src/liveConnection/onGift.cjs
@@ -1,5 +1,9 @@
-function onGift() {
+const searchParticipantInAllGroups = require('../utils/searchParticipantInAllGroups.cjs');
 
+function onGift({ uniqueId, giftId }) {
+    const giftPoints = { '5655': 100, '5658': 3e3, '5886': 3e5 };
+    const { group } = searchParticipantInAllGroups(uniqueId);
+    group.addPoints(uniqueId, giftPoints[giftId]);
 }
 
 module.exports = onGift;
