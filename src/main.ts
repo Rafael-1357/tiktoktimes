922539e2bef84e1d328dc8041c14bae74e4c7b18
Rafael Silveira <rafaellsilveira1357@gmail.com>
2023-12-27 04:17:56 -0300

    Adicionando Jquery.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 const socket = io("http://localhost:3001");
+import $ from 'jquery';
 
 socket.on('initialData', (data) => {
     console.log(data);
@@ -7,4 +8,9 @@ socket.on('initialData', (data) => {
 
 socket.on('add', (newParticipant) => {
     console.log(newParticipant);
+});
+
+$(document).ready(function(){
+			/* a função muda o background da div com id="box" */	
+		$("#content").css("background","#f00");
 });
